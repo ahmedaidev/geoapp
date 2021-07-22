9933a9d5031113182d1c15704b96d5ad901e05a7
Ahmed Ibrahim <ahmed.ibrahim@enacveneto.it>
2021-07-22 16:56:38 +0200

    BREAK(strapi): Change api uri

--- src/api/strapi.js
+++ src/api/strapi.js
@@ -1,9 +1,6 @@
 import axios from 'axios'
 
-const API_URL =
-  process.env.NODE_ENV === 'production'
-    ? process.env.REACT_APP_API_URI
-    : 'http://localhost:5000'
+const API_URL = 'http://192.168.20.48'
 
 export default axios.create({
   baseURL: API_URL,
